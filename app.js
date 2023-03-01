9157bf075d03bd9b6ec4868bf2b6955ec5e07287
mehdibh <mehdibhaddou@gmail.com>
2023-03-01 23:00:13 +0100

    Launch : setup dynamodb, get, getbyid

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const {getQuizzes, getQuiz} = require("./dynamodb/dynamo");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(8080, () => {
+    console.log("Server launched");
+})
+
+/* Get */
+app.get('/quiz', async (req, res) => {
+    try {
+        const quizzes = await getQuizzes();
+        res.json(quizzes);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+})
+
+app.get('/quiz/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const quiz = await getQuiz(id);
+        res.json(quiz);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+})
+
+
+/* TODO :
+    1. POST prompt & guidance
+    2. Call GPT3 and receive output
+    3. Store object in dynamoDB
+*/
+/* Post */
+app.post('/quiz', (req, res) => {
+    let data = req.body;
+    res.send(data.prompt);
+})
+
---
+++ dynamodb/dynamo.js
@@ -0,0 +1,67 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+require('dotenv').config();
+
+AWS.config.update({
+    region: process.env.AWS_DEFAULT_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = "quiz";
+
+/* Functions */
+const getQuiz = async (id) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+            id
+        }
+    };
+    return await dynamoClient.scan(params).promise();
+}
+
+const getQuizzes = async () => {
+    const params = {
+        TableName: TABLE_NAME
+    };
+    return await dynamoClient.scan(params).promise();
+}
+
+const createQuiz = async (quiz) => {
+    quiz.id = uuid.v1();
+    const params = {
+        TableName: TABLE_NAME,
+        Item: quiz
+    }
+    return await dynamoClient.put(params).promise();
+}
+
+const updateQuiz = async (quiz) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Item: quiz
+    }
+    return await dynamoClient.put(params).promise();
+}
+
+const deleteQuiz = async (id) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+            id
+        }
+    };
+    return await dynamoClient.delete(params).promise();
+}
+
+module.exports = {
+    dynamoClient,
+    getQuiz,
+    getQuizzes,
+    createQuiz,
+    updateQuiz,
+    deleteQuiz
+}
